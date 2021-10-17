a4e89b5329cf7d5630f94a0b9b851a65636503ad
Pro Tim <protimcbpiyas@gmail.com>
2021-10-18 00:13:34 +0600

    first commit after installing all

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBRD4LVEHhII49Nv6CGIvcqo3Dw740UwAI",
+    authDomain: "learn-to-rise.firebaseapp.com",
+    projectId: "learn-to-rise",
+    storageBucket: "learn-to-rise.appspot.com",
+    messagingSenderId: "707274200972",
+    appId: "1:707274200972:web:2e9522fc40b9bd056768a9"
+  };
+
+  export default firebaseConfig;
